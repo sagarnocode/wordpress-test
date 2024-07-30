695334f8cfa5cd382041dba4bbaf77a4a4f1ea7f
Sagar V <sagar@teamengine.me>
2024-07-30 13:08:01 +0530

    adde plugin and minor changes

--- wp-content/themes/test-new/template-parts/content/content-page.php
+++ wp-content/themes/test-new/template-parts/content/content-page.php
@@ -12,46 +12,49 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <div class="container">
+        <div class="formTitleWrap" style="display: flex; flex-direction: column; justify-content: center;">
+            <?php if (!is_front_page()): ?>
+            <!-- <header class="entry-header alignwide abc"> -->
+            <header class="d-flex headingWrap">
+                <?php get_template_part('template-parts/header/entry-header'); ?>
+                <?php twenty_twenty_one_post_thumbnail(); ?>
+            </header><!-- .entry-header -->
+            <?php elseif (has_post_thumbnail()): ?>
+            <header class="entry-header alignwide xyz">
+                <?php twenty_twenty_one_post_thumbnail(); ?>
+            </header><!-- .entry-header -->
+            <?php endif; ?>
 
-	<?php if ( ! is_front_page() ) : ?>
-		<header class="entry-header alignwide">
-			<?php get_template_part( 'template-parts/header/entry-header' ); ?>
-			<?php twenty_twenty_one_post_thumbnail(); ?>
-		</header><!-- .entry-header -->
-	<?php elseif ( has_post_thumbnail() ) : ?>
-		<header class="entry-header alignwide">
-			<?php twenty_twenty_one_post_thumbnail(); ?>
-		</header><!-- .entry-header -->
-	<?php endif; ?>
+            <div class="entry-content">
+                <?php
+			the_content();
 
-	<div class="entry-content">
-		<?php
-		the_content();
-
-		wp_link_pages(
-			array(
-				'before'   => '<nav class="page-links" aria-label="' . esc_attr__( 'Page', 'twentytwentyone' ) . '">',
-				'after'    => '</nav>',
-				/* translators: %: Page number. */
-				'pagelink' => esc_html__( 'Page %', 'twentytwentyone' ),
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer default-max-width">
-			<?php
+			wp_link_pages(
+				array(
+					'before' => '<nav class="page-links" aria-label="' . esc_attr__('Page', 'twentytwentyone') . '">',
+					'after' => '</nav>',
+					/* translators: %: Page number. */
+					'pagelink' => esc_html__('Page %', 'twentytwentyone'),
+				)
+			);
+			?>
+            </div><!-- .entry-content -->
+        </div>
+    </div>
+    <?php if (get_edit_post_link()): ?>
+    <footer class="entry-footer default-max-width">
+        <?php
 			edit_post_link(
 				sprintf(
 					/* translators: %s: Post title. Only visible to screen readers. */
-					esc_html__( 'Edit %s', 'twentytwentyone' ),
+					esc_html__('Edit %s', 'twentytwentyone'),
 					'<span class="screen-reader-text">' . get_the_title() . '</span>'
 				),
 				'<span class="edit-link">',
 				'</span>'
 			);
 			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
+    </footer><!-- .entry-footer -->
+    <?php endif; ?>
 </article><!-- #post-<?php the_ID(); ?> -->
--- wp-content/themes/test-new/template-parts/header/entry-header.php
+++ wp-content/themes/test-new/template-parts/header/entry-header.php
@@ -7,4 +7,5 @@
  * @since Twenty Twenty-One 1.0
  */
 
-the_title( '<h1 class="entry-title">', '</h1>' );
+// the_title('<h1 class="entry-title">', '</h1>');
+the_title('<h2 style="max-width: unset;">', '</h2>');
